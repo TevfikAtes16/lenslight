a8d98f613ba7d9a6ce122740d4f4353f6eaad511
Tevfik Ateş <tevfikates1@gmail.com>
2023-09-06 11:17:04 +0300

    Photo get and post events

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express  from "express";
 import dotenv  from "dotenv";
 import conn  from "./db.js";
+import pageRoute  from "./routes/pageRoute.js";
+import photoRoute  from "./routes/photoRoute.js";
 
 dotenv.config();
 
@@ -14,15 +16,19 @@ app.set('view engine','ejs');
 
 //Static Files Middleware
 app.use(express.static('public'));
+app.use(express.json());
 
+//Routes
+app.use('/',pageRoute);
+app.use('/photos',photoRoute);
 
-app.get('/', (req,res) => {
-    res.render('index');
-})
+// app.get('/', (req,res) => {
+//     res.render('index');
+// })
 
-app.get('/about', (req,res) => {
-    res.render('about');
-})
+// app.get('/about', (req,res) => {
+//     res.render('about');
+// })
 
 app.listen(process.env.PORT, () => {
     console.log(`Application running on port: ${process.env.HTTP}${process.env.PORT}/`);
