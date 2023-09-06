b2ee09d0b337d3ffd7ca18d6dd21241256864dc1
Tevfik Ateş <tevfikates1@gmail.com>
2023-09-06 14:20:55 +0300

    Register Page was become.

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-import express  from "express";
-import dotenv  from "dotenv";
-import conn  from "./db.js";
-import pageRoute  from "./routes/pageRoute.js";
-import photoRoute  from "./routes/photoRoute.js";
+import express from "express";
+import dotenv from "dotenv";
+import conn from "./db.js";
+import pageRoute from "./routes/pageRoute.js";
+import photoRoute from "./routes/photoRoute.js";
+import userRoute from "./routes/userRoute.js";
 
 dotenv.config();
 
@@ -12,15 +13,17 @@ conn();
 const app = express();
 
 //Ejs Template Engine
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 
 //Static Files Middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 //Routes
-app.use('/',pageRoute);
-app.use('/photos',photoRoute);
+app.use('/', pageRoute);
+app.use('/photos', photoRoute);
+app.use('/users', userRoute);
 
 // app.get('/', (req,res) => {
 //     res.render('index');
@@ -33,4 +36,3 @@ app.use('/photos',photoRoute);
 app.listen(process.env.PORT, () => {
     console.log(`Application running on port: ${process.env.HTTP}${process.env.PORT}/`);
 });
-
--- controllers/photoController.js
+++ controllers/photoController.js
@@ -17,9 +17,23 @@ const createPhoto = async (req, res) => {
 const getAllPhotos = async (req, res) => {
     try {
         const photos = await Photo.find({}).lean();
-        res.status(200).json({
-            succeded: true,
-            photos
+        res.status(200).render('photos',{
+            photos,
+            link:"photos"
+        });
+    } catch (error) {
+        res.status(500).json({
+            succeded:false,
+            error
+        });
+    }
+}
+const getAPhoto = async (req, res) => {
+    try {
+        const photo = await Photo.findById({ _id : req.params.id});
+        res.status(200).render('photo',{
+            photo,
+            link:"photos"
         });
     } catch (error) {
         res.status(500).json({
@@ -33,5 +47,6 @@ const getAllPhotos = async (req, res) => {
 
 export {
     createPhoto,
-    getAllPhotos
+    getAllPhotos,
+    getAPhoto
 };
